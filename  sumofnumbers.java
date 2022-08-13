30b4032c4041c9269914a3fa776f522795123d9a
ChiragWadhwani <wadhwanichirag94@gmail.com>
2022-08-13 11:04:29 +0000

    Simplified

---  sumofnumbers.java
+++  sumofnumbers.java
@@ -9,7 +9,7 @@ class sumofnumbers{
         int a =sc.nextInt();
         System.out.print("Enter 2nd number :");
         int b =sc.nextInt();
-        int sum = a+b;
-        System.out.println("Sum of given numbers is =" + sum);
+        
+        System.out.println("Sum of given numbers is =" + (a+b));
     }
 }
